041c4e2609c9fb5ca3f18c3bd29a246b9462a783
Gufran Khurshid <gufran.k@apollo247.org>
2026-01-30 22:31:54 -0500

    ELI-000 More file type constarint

--- backend/src/routes/testStrips.routes.ts
+++ backend/src/routes/testStrips.routes.ts
@@ -4,7 +4,17 @@ import * as ctrl from '../controllers/testStrips.controller';
 
 const router = Router();
 
-router.post('/upload', upload.single('image'), ctrl.uploadTestStrip);
+//router.post('/upload', upload.single('image'), ctrl.uploadTestStrip);
+
+router.post('/upload', (req, res) => {
+  upload.single('image')(req, res, (err) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    // IF NO ERROR, call your controller
+    ctrl.uploadTestStrip(req, res);
+  });
+});
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getOne);
 
