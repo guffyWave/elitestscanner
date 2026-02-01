0061ea5595b1e54aaca65f4398ad0124b8193e3c
Gufran Khurshid <gufran.k@apollo247.org>
2026-02-01 14:54:22 -0500

    ELI-000 Minor server change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Start server only when not running tests
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log('Backend running on port', PORT);
   });
 }
